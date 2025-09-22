11ab938e22f40f716a099e30a61c08c528382f02
Oksana Datskiv <oksana.datskiv.ri.2025@lpnu.ua>
2025-09-22 12:21:24 +0300

    first branch_02 commit

--- lab2.1/lab2.1.cpp
+++ lab2.1/lab2.1.cpp
@@ -11,20 +11,20 @@ using namespace std;
 
 int main() {
     double a;   // вхідний параметр (кут у радіанах)
-    double z1;  // результат першого виразу
-   // double z2;  // результат другого виразу
+    //double z1;  // результат першого виразу
+    double z2;  // результат другого виразу
 
     cout << "a = ";
     cin >> a;
 
     // Обчислення
-    z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
-   // z2 = 4 * cos(a / 2) * cos(5 * a / 2) * cos(4 * a);
+    //z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+    z2 = 4 * cos(a / 2) * cos(5 * a / 2) * cos(4 * a);
 
     // Вивід
     cout << endl;
-    cout << "z1 = " << z1 << endl;
-   // cout << "z2 = " << z2 << endl;
+    //cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 
     cin.get();
     return 0;
