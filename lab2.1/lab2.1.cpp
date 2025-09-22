24b228f9941e90500b3faf5d19654c2a8559a610
Oksana Datskiv <oksana.datskiv.ri.2025@lpnu.ua>
2025-09-22 12:12:43 +0300

    first master commit

---
+++ lab2.1/lab2.1.cpp
@@ -0,0 +1,31 @@
+// Lab_02.cpp
+// Дацьків Оксана
+// Лабораторна робота No 2.
+// Лінійні програми.
+// Варіант 6
+
+#include <iostream>
+#include <cmath>   // для cos()
+
+using namespace std;
+
+int main() {
+    double a;   // вхідний параметр (кут у радіанах)
+    double z1;  // результат першого виразу
+   // double z2;  // результат другого виразу
+
+    cout << "a = ";
+    cin >> a;
+
+    // Обчислення
+    z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+   // z2 = 4 * cos(a / 2) * cos(5 * a / 2) * cos(4 * a);
+
+    // Вивід
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+   // cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
